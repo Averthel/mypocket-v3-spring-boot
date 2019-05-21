d17c3fd457f6f431362e9dabb8e8ebf7aed82045
Michał <42038384+Averthel@users.noreply.github.com>
2019-05-21 23:22:31 +0200

    Security Form

--- src/main/java/pl/mypocket/controller/web/HomeController.java
+++ src/main/java/pl/mypocket/controller/web/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.mypocket.model.User;
 
 import javax.validation.Valid;
@@ -28,6 +29,16 @@ public class HomeController {
             List<ObjectError> errors = result.getAllErrors();
             errors.forEach(err -> System.out.println(err.getDefaultMessage()));
         }
-        return  "index";
+        return "index";
+    }
+
+    @RequestMapping("/userpanel")
+    public String userPanel(){
+        return "userpanel";
+    }
+
+    @GetMapping("/loginform")
+    public String loginForm(){
+        return "login_form";
     }
 }
---
+++ src/main/java/pl/mypocket/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package pl.mypocket.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/").permitAll().antMatchers("/add").permitAll().anyRequest().authenticated().and()
+                .formLogin().loginPage("/loginform").permitAll()
+                .loginProcessingUrl("/processlogin").permitAll()
+                .usernameParameter("user")
+                .passwordParameter("pass")
+                .and()
+                .logout()
+                .logoutUrl("/logmeout")
+                .logoutSuccessUrl("/").permitAll();
+    }
+}
